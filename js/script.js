601b3288ac2cbdc5edf4312081be1c44dc3915d4
Ariel Michelli <arielm-04@hotmail.com>
2022-10-13 18:09:45 -0300

    agregando funciones escribir y borrar monto

--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
-const btn = document.querySelector('button');
-
-function random(number) {
-    return Math.floor(Math.random() * (number+1));
-}
-
-btn.onclick = function() {
-    const random_color = 'rgb('+ random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = random_color;
-    console.log(`${random_color}`)
-}
-
-console.log("hola pepe")
-
-const input = document.querySelector('input');
-const log = document.getElementById('valores');
-
-input.addEventListener('input', updateValue);
-console.log
-
-function updateValue(e) {
-  log.textContent = e.srcElement.value;
-}
-
 var monto = 0;
+const precio = 200;
 
 function capturar(){
     var nombre = document.getElementById('nombre').value
@@ -32,7 +9,6 @@ function capturar(){
     var cantidad_entradas = document.getElementById('cantidad').value;
     var categoria = document.getElementById('categoria').value;
     var descuento = 0;
-    const precio = 200;
 
     if (categoria == "Estudiante"){
         descuento = 0.8;
@@ -40,15 +16,24 @@ function capturar(){
     else if(categoria == "Trainee"){
         descuento = 0.5;
     }
-    else{
+    else if(categoria == "Junior"){
         descuento = 0.15;
     }
+    else {
+        descuento = 0;
+    }
     monto = Math.round(cantidad_entradas *  (1 - descuento) * precio);
-    // updateValue(monto);
+    escribir_monto();
+
+    console.log(`Los tickets estan a nombre de: ${nombre} con apellido: ${apellido} y un mail: ${correo}`)
     console.log(`Por ser :${categoria} el descuento es del : ${descuento * 100}% El monto total es: ${monto}`)
-    escribir();
 }
 
-function escribir(){
-    document.getElementById('monto').innerHTML=`El Monto total es: ${monto}`;
-    }
+function escribir_monto(){
+    document.getElementById('monto').innerHTML=`${monto}`;
+}
+
+function borrar_monto(){
+    monto = 0;
+    escribir_monto();
+}
